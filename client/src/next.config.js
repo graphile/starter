7485dc5478c7cd4fe957fd6f57ed97b679dc9f00
Benjie Gillam <benjie@jemjie.com>
2019-04-20 12:09:39 +0100

    NProgress loading indicator

--- client/src/next.config.js
+++ client/src/next.config.js
@@ -1,4 +1,6 @@
+const { compose } = require("react-apollo");
 module.exports = () => {
+  const withCss = require("@zeit/next-css");
   const withLess = require("@zeit/next-less");
   const withTypeScript = require("@zeit/next-typescript");
   const lessToJS = require("less-vars-to-js");
@@ -15,43 +17,43 @@ module.exports = () => {
   if (typeof require !== "undefined") {
     require.extensions[".less"] = () => {};
   }
-  return withTypeScript(
-    withLess({
-      distDir: "../../.next",
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-        modifyVars: themeVariables, // make your antd custom effective
-      },
-      webpack(config, { webpack, dev, dir }) {
-        if (dev) config.devtool = "cheap-module-source-map";
+  return compose(
+    withCss,
+    withTypeScript,
+    withLess
+  )({
+    distDir: "../../.next",
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables, // make your antd custom effective
+    },
+    webpack(config, { webpack, dev, dir }) {
+      if (dev) config.devtool = "cheap-module-source-map";
 
-        const graphqlRule = {
-          test: /\.(graphql|gql)$/,
-          include: [dir],
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "graphql-tag/loader",
-            },
-          ],
-        };
-        return {
-          ...config,
-          module: {
-            ...config.module,
-            rules: [...config.module.rules, graphqlRule],
+      const graphqlRule = {
+        test: /\.(graphql|gql)$/,
+        include: [dir],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "graphql-tag/loader",
           },
-          plugins: [
-            ...config.plugins,
-            new webpack.DefinePlugin({
-              "process.env.ROOT_DOMAIN": JSON.stringify(
-                process.env.ROOT_DOMAIN
-              ),
-              "process.env.ROOT_URL": JSON.stringify(process.env.ROOT_URL),
-            }),
-          ],
-        };
-      },
-    })
-  );
+        ],
+      };
+      return {
+        ...config,
+        module: {
+          ...config.module,
+          rules: [...config.module.rules, graphqlRule],
+        },
+        plugins: [
+          ...config.plugins,
+          new webpack.DefinePlugin({
+            "process.env.ROOT_DOMAIN": JSON.stringify(process.env.ROOT_DOMAIN),
+            "process.env.ROOT_URL": JSON.stringify(process.env.ROOT_URL),
+          }),
+        ],
+      };
+    },
+  });
 };
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,37 @@
 import "antd/dist/antd.less";
+import "nprogress/nprogress.css";
 import * as React from "react";
 import App, { Container } from "next/app";
 import { ApolloProvider } from "react-apollo";
 import withApollo from "../lib/withApollo";
 import { ApolloClient } from "apollo-client";
+import Router from "next/router";
+import { notification } from "antd";
+import NProgress from "nprogress";
+
+NProgress.configure({
+  showSpinner: false,
+});
+
+if (typeof window !== "undefined") {
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
+  Router.events.on("routeChangeError", (err: Error | string) => {
+    NProgress.done();
+    notification.open({
+      message: "Page load failed",
+      description: `This is very embarassing! Please reload the page. Further error details: ${
+        typeof err === "string" ? err : err.message
+      }`,
+      duration: 0,
+    });
+  });
+}
 
 class MyApp extends App<{ apollo: ApolloClient<any> }> {
   static async getInitialProps({ Component, ctx }: any) {
