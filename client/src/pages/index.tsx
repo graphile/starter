af69da658f2112f3a204b1d57ec8698f39f132c3
Benjie Gillam <benjie@jemjie.com>
2019-05-23 13:23:00 +0100

    Welcome page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,85 +8,90 @@ export default function Home() {
     <SharedLayout title="Home">
       <h2>Welcome to PostGraphile Starter</h2>
       <p>
-        This project can server as a basis for your own project. We've added a
-        number of things that most projects require, but you're free to pull
-        them out and switch them with whatever you need.
+        This project can serve as a basis for your own project. We've added a
+        things that most projects require, but you're free to remove them or
+        replace them with whatever you need.
       </p>
-      <ul>
-        <li>
-          <strong>TypeScript</strong> — make your project maintainable by
-          starting with strong typing throughout
-        </li>
-        <li>
-          <strong>Ant Design</strong> — &ldquo;A design system with values of
-          Nature and Determinacy for better user experience of enterprise
-          applications&rdquo;
-        </li>
-        <li>
-          <strong>Hot reloading</strong> — edit this file
-          `client/src/pages/index.tsx` and when you save it should update in
-          your browser immediately
-        </li>
-        <li>
-          <strong>SSR with hot reloading</strong> — if you turn JS off and
-          reload the page you should be greeted with the same content anyway
-        </li>
-        <li>
-          <strong>Prettier</strong> — consistent automated formatting for your
-          code
-        </li>
-        <li>
-          <strong>Linting</strong> — provided by ESLint, can be extended with
-          extremely powerful lint rules to protect your project from bugs, bad
-          practices, and inconsistencies
-        </li>
-        <li>
-          <strong>Migrations</strong> — we use{" "}
-          <a href="https://github.com/graphile/migrate">graphile-migrate</a> but
-          you're welcome to switch to whatever migration library you like
-        </li>
-        <li>
-          <strong>Job queue</strong> — we've added{" "}
-          <a href="https://github.com/graphile/worker">graphile-worker</a> to
-          perform background tasks for you, such as sending emails
-        </li>
-        <li>
-          <strong>Responsive email templates</strong> — we've added MJML and a
-          templating system to help you format emails; all emails go through the
-          `send_email` task so it's easy to customise
-        </li>
-        <li>
-          <strong>Email transport</strong> — we've configured nodemailer to use
-          ethereal.email to catch all development emails, and AWS' SES in
-          production (easy to customise)
-        </li>
-        <li>
-          <strong>HTML 2 text emails</strong> — automated conversion from your
-          pretty templates to plain text emails for users that prefer that
-        </li>
-        <li>
-          <strong>Express server</strong> — with easy to customise middleware
-          system
-        </li>
-        <li>
-          <strong>Sessions</strong> — to persist login information in the
-          traditional way (with cookies)
-        </li>
-        <li>
-          <strong>Realtime</strong> — PostGraphile is configured with
-          @graphile/pg-pubsub to enable realtime events from the DB; and Apollo
-          is configured to consume them
-        </li>
-        <li>
-          <strong>GraphQL</strong> — on the backend through PostGraphile, on the
-          frontend via Apollo client
-        </li>
-        <li>
-          <strong>User system</strong> — supporting both username/password
-          registration and OAuth / social authentication (Twitter, GitHub,
-          Facebook, ...)
-        </li>
-      </ul>
+      <h3>Page load delays</h3>
+      <p>
+        We use Next.js to control the React app. This gives us server-side
+        rendering, routing, bundle splitting, hot reloading, and much more.
+        However, this does mean that in development when you visit a new URL it
+        must first be loaded from the file system and transpiled by the server,
+        served to the client, and then executed. This means there can be a small
+        delay when loading a web page for the first time in development. In
+        production, this delay will be much smaller, and can be eliminated with
+        prefetching. You can read more about this in the{" "}
+        <a href="https://nextjs.org/docs#prefetching-pages">Next.js docs</a>
+      </p>
+      <h3>Registration</h3>
+      <p>
+        To get started, you'll probably want to try the login/registration flow.{" "}
+        <strong>
+          In development we don't send any emails to real email addresses
+        </strong>
+        , instead all emails are sent to ethereal.email where you can browse
+        them. Keep an eye on the terminal that's running the server, when an
+        email is triggered it will present you with a URL you can visit to view
+        the email. We've configured the project to use Amazon SES to send emails
+        in production (you will need to create the relevant resources and
+        provide the relevant secrets to use this); however it can easily be
+        reconfigured to use your email transport service of choice.
+      </p>
+      <p>
+        To get started, click "login" at the top right, then choose "Register
+        Here".
+      </p>
+      <p>
+        NOTE: to prevent people blocking legitimate users from registering, we
+        only prevent the same email address being used multiple times when it
+        has been verified by someone. It's up to your application to turn
+        features off/etc when the user does not have any verified email
+        addresses (should you desire this).
+      </p>
+      <h3>Validation</h3>
+      <p>
+        We use <a href="https://ant.design/components/form/">AntD's forms</a>,
+        so validation is provided via these. We've shown how to connect
+        server-side errors into the form validation, for example try registering
+        a new account using the email address of an account{" "}
+        <strong>that has already been verified</strong>.
+      </p>
+      <h3>This file</h3>
+      <p>
+        This file is located in the client pages folder:
+        <code>client/src/pages/index.tsx</code>. If you edit it and save it
+        should update in your browser immediately. Further if you disable JS and
+        reload the page you should see the content is still displayed (this is
+        called "server-side rendering" and is important to ensuring that your
+        users have the best low-latency experience of your website, and that
+        search engines can index its content).
+      </p>
+      <h3>The server</h3>
+      <p>
+        The entry point to the server is <code>server/src/server/index.ts</code>
+        ; this creates an Express.js server and installs a number of
+        middlewares, including PostGraphile.
+      </p>
+      <p>
+        To make sessions persistent (which you definitely want, because we
+        restart the server whenever server files change), we've configured
+        Express to use Redis as a session store. You need to ensure redis is
+        installed and running locally, or disable the <code>REDIS_URL</code>{" "}
+        configuration variable in <code>.env</code>.
+      </p>
+      <h3>Realtime</h3>
+      <p>
+        We've configured PostGraphile with @graphile/pg-pubsub to enable
+        realtime events from the DB; and Apollo is configured to consume them.
+        TODO: how to demonstrate this?
+      </p>
+      <h3>What now?</h3>
+      <p>
+        If you're happy with our user account system, we recommend that you
+        commit this first migration with <code>yarn db:migrate commit</code>,
+        and then set about making this application your own.
+      </p>
     </SharedLayout>
   );
 }
