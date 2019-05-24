635a23f0edf78bd2e98cc02ceca63933dd49ce92
Benjie Gillam <benjie@jemjie.com>
2019-05-24 12:00:50 +0100

    Starter domain change

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
 const packageJson = require("../../package.json");
 
-export const fromEmail = '"PostGraphile Starter" <no-reply@example.com>';
+export const fromEmail =
+  '"PostGraphile Starter" <no-reply@examples.graphile.org>';
 export const awsRegion = "us-east-1";
 export const companyName = packageJson.name;
 export const legalText = "<Insert legal email footer text here >";
