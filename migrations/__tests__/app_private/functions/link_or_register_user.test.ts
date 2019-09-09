5e526af34f86ff315b5829e334faa9f840b90cee
Benjie Gillam <benjie@jemjie.com>
2019-09-09 16:46:06 +0100

    link_or_register_user tests

--- migrations/__tests__/app_private/functions/link_or_register_user.test.ts
+++ migrations/__tests__/app_private/functions/link_or_register_user.test.ts
@@ -1,10 +1,6 @@
 import { withRootDb, snapshotSafe, deleteTestUsers } from "../../helpers";
 import { PoolClient } from "pg";
 
-beforeEach(deleteTestUsers);
-
-// Note: registration has already been tested, so we should assume account already exists
-
 async function linkOrRegisterUser(
   client: PoolClient,
   userId: number | null,
@@ -28,28 +24,31 @@ async function linkOrRegisterUser(
   return row;
 }
 
-it("can login with full oauth details", () =>
-  withRootDb(async client => {
-    const user = await linkOrRegisterUser(
-      client,
-      null,
-      "github",
-      "123456",
-      {
-        email: "github.user.123456@example.com",
-        name: "GitHub User123456",
-        avatar_url: "http://example.com/avatar.jpg",
-        username: "GHU123",
-      },
-      {}
-    );
-    expect(user).toBeTruthy();
-    expect(user.username).toEqual("GHU123");
-    expect(user.name).toEqual("GitHub User123456");
-    expect(user.avatar_url).toEqual("http://example.com/avatar.jpg");
-    expect(user.is_admin).toEqual(false);
-    expect(user.is_verified).toEqual(true);
-    expect(snapshotSafe(user)).toMatchInlineSnapshot(`
+describe("when account doesn't already exist", () => {
+  beforeEach(deleteTestUsers);
+
+  it("can login with full oauth details", () =>
+    withRootDb(async client => {
+      const user = await linkOrRegisterUser(
+        client,
+        null,
+        "github",
+        "123456",
+        {
+          email: "github.user.123456@example.com",
+          name: "GitHub User123456",
+          avatar_url: "http://example.com/avatar.jpg",
+          username: "GHU123",
+        },
+        {}
+      );
+      expect(user).toBeTruthy();
+      expect(user.username).toEqual("GHU123");
+      expect(user.name).toEqual("GitHub User123456");
+      expect(user.avatar_url).toEqual("http://example.com/avatar.jpg");
+      expect(user.is_admin).toEqual(false);
+      expect(user.is_verified).toEqual(true);
+      expect(snapshotSafe(user)).toMatchInlineSnapshot(`
       Object {
         "avatar_url": "http://example.com/avatar.jpg",
         "created_at": "[DATE]",
@@ -61,73 +60,118 @@ it("can login with full oauth details", () =>
         "username": "GHU123",
       }
     `);
-  }));
+    }));
 
-it("can login with minimal oauth details", () =>
+  it("can login with minimal oauth details", () =>
+    withRootDb(async client => {
+      const user = await linkOrRegisterUser(
+        client,
+        null,
+        "github",
+        "123456",
+        {
+          email: "github.user.123456@example.com",
+        },
+        {}
+      );
+      expect(user).toBeTruthy();
+      expect(user.username).toMatch(/^user(?:[1-9][0-9]+)?$/);
+      expect(user.name).toEqual(null);
+      expect(user.avatar_url).toEqual(null);
+      expect(user.is_admin).toEqual(false);
+      expect(user.is_verified).toEqual(true);
+    }));
+
+  test("cannot register without email", () =>
+    withRootDb(async client => {
+      const promise = client.query(
+        "SELECT * FROM app_private.link_or_register_user($1, $2, $3, $4, $5)",
+        [
+          null,
+          "facebook",
+          "123456",
+          JSON.stringify({ email: null, firstName: "A", lastName: "B" }),
+          JSON.stringify({}),
+        ]
+      );
+      await expect(promise).rejects.toMatchInlineSnapshot(
+        `[error: Email is required]`
+      );
+      await expect(promise).rejects.toMatchObject({
+        code: "MODAT",
+      });
+    }));
+
+  it("cannot register with invalid email", () =>
+    withRootDb(async client => {
+      const promise = linkOrRegisterUser(
+        client,
+        null,
+        "github",
+        "123456",
+        { email: "flibble" },
+        {}
+      );
+      await expect(promise).rejects.toThrowErrorMatchingInlineSnapshot(
+        `"new row for relation \\"user_emails\\" violates check constraint \\"user_emails_email_check\\""`
+      );
+      await expect(promise).rejects.toMatchObject({
+        code: "23514",
+      });
+    }));
+});
+
+it("login with new oauth sharing email of existing account links accounts", () =>
   withRootDb(async client => {
-    const user = await linkOrRegisterUser(
+    const sharedEmail = "existing@example.com";
+    const existingUser = await linkOrRegisterUser(
       client,
       null,
       "github",
       "123456",
       {
-        email: "github.user.123456@example.com",
+        email: sharedEmail,
       },
       {}
     );
-    expect(user).toBeTruthy();
-    expect(user.username).toMatch(/^user(?:[1-9][0-9]+)?$/);
-    expect(user.name).toEqual(null);
-    expect(user.avatar_url).toEqual(null);
-    expect(user.is_admin).toEqual(false);
-    expect(user.is_verified).toEqual(true);
+    expect(existingUser).toBeTruthy();
+    const linkedUser = await linkOrRegisterUser(
+      client,
+      null,
+      "twitter",
+      "654321",
+      {
+        email: sharedEmail,
+      },
+      {}
+    );
+    expect(linkedUser).toBeTruthy();
+    expect(existingUser.id).toEqual(linkedUser.id);
   }));
 
-test("cannot register without email", () =>
+it("login with new oauth when logged in links accounts", () =>
   withRootDb(async client => {
-    const promise = client.query(
-      "SELECT * FROM app_private.link_or_register_user($1, $2, $3, $4, $5)",
-      [
-        null,
-        "facebook",
-        "123456",
-        JSON.stringify({ email: null, firstName: "A", lastName: "B" }),
-        JSON.stringify({}),
-      ]
-    );
-    await expect(promise).rejects.toMatchInlineSnapshot(
-      `[error: Email is required]`
-    );
-    await expect(promise).rejects.toMatchObject({
-      code: "MODAT",
-    });
-  }));
-
-it("cannot register with invalid email", () =>
-  withRootDb(async client => {
-    const promise = linkOrRegisterUser(
+    const githubUser = await linkOrRegisterUser(
       client,
       null,
       "github",
       "123456",
-      { email: "flibble" },
+      {
+        email: "github@example.com",
+      },
       {}
     );
-    await expect(promise).rejects.toThrowErrorMatchingInlineSnapshot(
-      `"new row for relation \\"user_emails\\" violates check constraint \\"user_emails_email_check\\""`
+    expect(githubUser).toBeTruthy();
+    const twitterUser = await linkOrRegisterUser(
+      client,
+      githubUser.id,
+      "twitter",
+      "654321",
+      {
+        email: "twitter@example.com",
+      },
+      {}
     );
-    await expect(promise).rejects.toMatchObject({
-      code: "23514",
-    });
+    expect(twitterUser).toBeTruthy();
+    expect(twitterUser.id).toEqual(githubUser.id);
   }));
-
-it.todo("can login with username+password");
-it.todo("can login with uSeRnAmE+password");
-it.todo("can login with email+password");
-it.todo("can login with EmAiL+password");
-it.todo("cannot login with wrong password");
-it.todo("prevents too many login attempts");
-it.todo(
-  "login with new oauth sharing email of existing account links accounts"
-);
-it.todo("login with new oauth when logged in links accounts");
---
+++ migrations/__tests__/app_private/functions/login.test.ts
@@ -0,0 +1,6 @@
+it.todo("can login with username+password");
+it.todo("can login with uSeRnAmE+password");
+it.todo("can login with email+password");
+it.todo("can login with EmAiL+password");
+it.todo("cannot login with wrong password");
+it.todo("prevents too many login attempts");
