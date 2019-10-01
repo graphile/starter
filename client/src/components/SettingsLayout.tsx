db24af07fad19f579f73ace11a570a8ecc7eb0a2
Benjie Gillam <benjie@jemjie.com>
2019-10-01 17:27:49 +0100

    All settings pages require currentUser

--- client/src/components/SettingsLayout.tsx
+++ client/src/components/SettingsLayout.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { Layout, Menu } from "antd";
 import StandardWidth from "./StandardWidth";
 import Warn from "./Warn";
+import Redirect from "./Redirect";
 
 const { Sider, Content } = Layout;
 
@@ -42,34 +43,38 @@ export default function SettingsLayout({
   const page = pages[href];
   return (
     <SharedLayout title={`Settings: ${page.title}`} noPad>
-      {({ currentUser }: SharedLayoutChildProps) => (
-        <Layout style={{ minHeight: "calc(100vh - 64px - 64px)" }} hasSider>
-          <Sider>
-            <Menu selectedKeys={[href]}>
-              {Object.keys(pages).map(href => (
-                <Menu.Item key={href}>
-                  <Link href={href}>
-                    <a data-cy={pages[href].cy}>
-                      <Warn
-                        okay={
-                          !currentUser ||
-                          currentUser.isVerified ||
-                          !pages[href].warnIfUnverified
-                        }
-                      >
-                        {pages[href].title}
-                      </Warn>
-                    </a>
-                  </Link>
-                </Menu.Item>
-              ))}
-            </Menu>
-          </Sider>
-          <Content>
-            <StandardWidth>{children}</StandardWidth>
-          </Content>
-        </Layout>
-      )}
+      {({ currentUser, error, loading }: SharedLayoutChildProps) =>
+        !currentUser && !error && !loading ? (
+          <Redirect href={`/login?next=${encodeURIComponent(inHref)}`} />
+        ) : (
+          <Layout style={{ minHeight: "calc(100vh - 64px - 64px)" }} hasSider>
+            <Sider>
+              <Menu selectedKeys={[href]}>
+                {Object.keys(pages).map(href => (
+                  <Menu.Item key={href}>
+                    <Link href={href}>
+                      <a data-cy={pages[href].cy}>
+                        <Warn
+                          okay={
+                            !currentUser ||
+                            currentUser.isVerified ||
+                            !pages[href].warnIfUnverified
+                          }
+                        >
+                          {pages[href].title}
+                        </Warn>
+                      </a>
+                    </Link>
+                  </Menu.Item>
+                ))}
+              </Menu>
+            </Sider>
+            <Content>
+              <StandardWidth>{children}</StandardWidth>
+            </Content>
+          </Layout>
+        )
+      }
     </SharedLayout>
   );
 }
