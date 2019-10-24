314505b487b831cf3b3a70fc9a6db3b1994cb235
Benjie Gillam <benjie@jemjie.com>
2019-10-24 19:31:34 +0100

    Remove old file

--- @app/config/src/db.ts
+++
@@ -1,9 +0,0 @@
-// These are the connection strings for the DB and the test DB.
-// NOTE: in production you probably want to add ?ssl=1 to force SSL usage.
-// NOTE: 'psql' does not like ?ssl=1, use ?sslmode=require for psql.
-// NOTE: these used to be in `.env` but now it is used by docker-compose we can't use expansions
-export const DATABASE_URL = `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
-export const AUTH_DATABASE_URL = `postgres://${process.env.DATABASE_AUTHENTICATOR}:${process.env.DATABASE_AUTHENTICATOR_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
-export const SHADOW_DATABASE_URL = `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_shadow`;
-export const SHADOW_AUTH_DATABASE_URL = `postgres://${process.env.DATABASE_AUTHENTICATOR}:${process.env.DATABASE_AUTHENTICATOR_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_shadow`;
-export const TEST_DATABASE_URL = `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_test`;
