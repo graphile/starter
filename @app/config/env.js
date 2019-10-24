35ad166d678d5600932ddae7b7d3c1e7835dedd5
Benjie Gillam <benjie@jemjie.com>
2019-10-24 20:22:25 +0100

    Cypress tests run again

--- @app/config/env.js
+++ @app/config/env.js
@@ -20,3 +20,6 @@ process.env.SHADOW_AUTH_DATABASE_URL =
 process.env.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
   `postgres://${process.env.DATABASE_OWNER}:${process.env.DATABASE_OWNER_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}_test`;
+
+// https://docs.cypress.io/guides/guides/environment-variables.html#Option-3-CYPRESS
+process.env.CYPRESS_ROOT_URL = process.env.ROOT_URL;
