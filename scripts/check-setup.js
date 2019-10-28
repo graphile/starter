9bc85c505bbe7af78d2befe1097f7760daccc1e2
Benjie Gillam <benjie@jemjie.com>
2019-10-28 18:35:58 +0000

    Docker mode or local mode

--- scripts/check-setup.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const fs = require("fs");
-
-if (!fs.existsSync(`${__dirname}/../.env`)) {
-  console.error("🛠️  Please run 'yarn setup' before running 'yarn start'");
-  process.exit(1);
-}
---
+++ scripts/start.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const { exec } = require("child_process");
+
+const ENVFILE = `${__dirname}/../.env`;
+
+if (!fs.existsSync(ENVFILE)) {
+  console.error("🛠️  Please run 'yarn setup' before running 'yarn start'");
+  process.exit(1);
+}
+const content = "\n" + fs.readFileSync(ENVFILE, "utf8");
+const matches = content.match(/\nDOCKER_MODE=(.*)/);
+if (matches && matches[1] === 'y') {
+  console.error();
+  console.error();
+  console.error("This repo was configured for docker mode, it cannot be started with yarn start - instead, try:");
+  console.error();
+  console.error('  export UID; docker-compose up')
+  console.error();
+  console.error();
+  process.exit(1);
+} else {
+  exec("yarn", ["dev"], {stdio: 'inherit'});
+}
