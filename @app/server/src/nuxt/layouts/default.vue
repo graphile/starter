4b045d9bc2c36e5ed106db58f7d36f905e7d1a40
Joe Schr <joe.vandelay@gmail.com>
2019-11-13 18:56:23 +0000

    wip, adapt layout from Next starter

--- @app/server/src/nuxt/layouts/default.vue
+++ @app/server/src/nuxt/layouts/default.vue
@@ -1,10 +1,69 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <Layout>
+    <Header>
+      <Row type="flex" justify="space-between">
+        <Col span="6">
+          <NuxtLink to="/">Home</NuxtLink>
+        </Col>
+        <Col>
+          <h3>{{ title }} - {{ projectName }}</h3>
+        </Col>
+      </Row>
+    </Header>
+    <Content> <Nuxt /> </Content>
+    <Footer
+      style="display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'"
+    >
+      <div>
+        <div>
+          Copyright &copy; {{ companyName }}. All rights reserved.
+
+          <span v-if="T_AND_C_URL">
+            <a :href="T_AND_C_URL">Terms and conditions</a>
+          </span>
+        </div>
+        <div>
+          Powered by
+          <a
+            style="color: '#fff', textDecoration: 'underline'"
+            href="https://graphile.org/postgraphile"
+            >PostGraphile</a
+          >
+        </div>
+      </div>
+    </Footer>
+  </Layout>
 </template>
 
-<style scoped>
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+// import { NuxtLink } from "@nuxt/types";
+import { Layout, Avatar, Row, Col, Dropdown, Icon, Menu } from "ant-design-vue";
+import { projectName, companyName } from "@app/config";
+
+@Component({
+  components: {
+    Layout,
+    Header: Layout.Header,
+    Avatar,
+    Row,
+    Col,
+    Dropdown,
+    Icon,
+    Menu,
+  },
+})
+export default class DefaultLayout extends Vue {
+  data() {
+    return {
+      projectName,
+      companyName,
+      T_AND_C_URL: process.env.T_AND_C_URL,
+    };
+  }
+}
+</script>
+<style lang="less" scoped>
 img {
   position: fixed;
   top: 20px;
---
+++ @app/server/src/nuxt/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h1 class="header">Index</h1>
+    <div class="cards">
+      <CustomCard v-for="person in people" :key="person.id" :person="person"></CustomCard>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
+import { Person } from "~/types";
+import CustomCard from "~/components/CustomCard.vue";
+
+@Component({
+  components: {
+    CustomCard,
+  },
+})
+export default class extends Vue {
+  @State people!: Person;
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
