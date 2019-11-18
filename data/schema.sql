bcc4a198223ad80d8df5a2d5b10b1f20817421d6
Benjie Gillam <benjie@jemjie.com>
2019-11-18 17:14:33 +0000

    Consistent Docker dump

--- data/schema.sql
+++ data/schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.5
--- Dumped by pg_dump version 11.5
+-- Dumped from database version 11.5 (Debian 11.5-1.pgdg90+1)
+-- Dumped by pg_dump version 11.5 (Debian 11.5-1.pgdg90+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
