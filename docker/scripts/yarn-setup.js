7ea904ee9b9798ab128b272399c2ce1e2c9a73fb
Joe Schr <joe.vandelay@gmail.com>
2019-11-19 19:46:05 +0100

    Fix: coding style

--- docker/scripts/yarn-setup.js
+++ docker/scripts/yarn-setup.js
@@ -1,7 +1,8 @@
 const { execSync, spawnSync } = require("child_process");
 const { basename, dirname, resolve } = require("path");
+const platform = require("os").platform();
 
-let options = { stdio: "inherit" };
+const options = { windowsHide: true, stdio: "inherit" };
 
 // The `docker-compose` project name defaults to the directory name containing
 // `docker-compose.yml`, which is the root folder of our project. Let's call
@@ -10,11 +11,9 @@ let options = { stdio: "inherit" };
 const projectName = basename(dirname(dirname(resolve(__dirname))));
 
 // fixes spawnSync not throwing ENOENT on windows
-const platform = require("os").platform();
 const yarnCmd = platform === "win32" ? "yarn.cmd" : "yarn";
 execSync(`${yarnCmd} down`, options);
 execSync(`${yarnCmd} db:up`, options);
-options = { windowsHide: true, ...options };
 spawnSync(
   yarnCmd,
   ["compose", "run", "server", "yarn", "setup", projectName],
