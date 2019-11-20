58f6d3d3c07e9e76e4dfea20352c89f18ce9c63b
Benjie Gillam <benjie@jemjie.com>
2019-11-20 17:12:06 +0000

    Error handling

--- docker/scripts/yarn-setup.js
+++ docker/scripts/yarn-setup.js
@@ -1,8 +1,20 @@
-const { spawnSync } = require("child_process");
+const { spawnSync: baseSpawnSync } = require("child_process");
 const { basename, dirname, resolve } = require("path");
 const platform = require("os").platform();
 
-const options = { stdio: "inherit", windowsHide: true };
+function spawnSync(command, args, options = {}) {
+  const result = baseSpawnSync(command, args, {
+    stdio: "inherit",
+    windowsHide: true,
+    ...options,
+  });
+  if (result.status) {
+    process.exit(result.status);
+  }
+  if (result.signal) {
+    process.exit(1);
+  }
+}
 
 // The `docker-compose` project name defaults to the directory name containing
 // `docker-compose.yml`, which is the root folder of our project. Let's call
@@ -13,10 +25,6 @@ const projectName = basename(dirname(dirname(resolve(__dirname))));
 // On Windows we must run 'yarn.cmd' rather than 'yarn'
 const yarnCmd = platform === "win32" ? "yarn.cmd" : "yarn";
 
-spawnSync(yarnCmd, ["down"], options);
-spawnSync(yarnCmd, ["db:up"], options);
-spawnSync(
-  yarnCmd,
-  ["compose", "run", "server", "yarn", "setup", projectName],
-  options
-);
+spawnSync(yarnCmd, ["down"]);
+spawnSync(yarnCmd, ["db:up"]);
+spawnSync(yarnCmd, ["compose", "run", "server", "yarn", "setup", projectName]);
