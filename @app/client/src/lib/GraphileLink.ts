a12f0f6b5b97c9af0ec38bf73e99bc92cda68403
Benjie Gillam <benjie@jemjie.com>
2019-12-02 15:01:27 +0000

    Disable subscriptions during SSR

--- @app/client/src/lib/GraphileLink.ts
+++ @app/client/src/lib/GraphileLink.ts
@@ -6,7 +6,7 @@ import {
   Observable,
 } from "apollo-link";
 import { HttpRequestHandler } from "postgraphile";
-import { execute } from "graphql";
+import { execute, getOperationAST } from "graphql";
 import { Request, Response } from "express";
 
 interface GraphileLinkInterface {
@@ -40,6 +40,14 @@ export default class GraphileLink extends ApolloLink {
     return new Observable(observer => {
       (async () => {
         try {
+          const op = getOperationAST(operation.query, operation.operationName);
+          if (!op || op.operation !== "query") {
+            if (!observer.closed) {
+              /* Only do queries (not subscriptions) on server side */
+              observer.complete();
+            }
+            return;
+          }
           const schema = await postgraphileMiddleware.getGraphQLSchema();
           const data = await postgraphileMiddleware.withPostGraphileContextFromReqRes(
             req,
--- @app/client/src/lib/withApollo.ts
+++ @app/client/src/lib/withApollo.ts
@@ -2,21 +2,55 @@ import withApollo from "next-with-apollo";
 import { ApolloLink, split } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import { WebSocketLink } from "apollo-link-ws";
-import { getMainDefinition } from "apollo-utilities";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { onError } from "apollo-link-error";
 import ws from "ws";
-import { pick } from "lodash";
 import GraphileLink from "./GraphileLink";
+import { getOperationAST } from "graphql";
+
+function makeServerSideLink(req: any, res: any) {
+  return new GraphileLink({
+    req,
+    res,
+    postgraphileMiddleware: req.app.get("postgraphileMiddleware"),
+  });
+}
+
+function makeClientSideLink(ROOT_URL: string) {
+  const httpLink = new HttpLink({
+    uri: `${ROOT_URL}/graphql`,
+    credentials: "same-origin",
+  });
+  const wsLink = new WebSocketLink({
+    uri: `${ROOT_URL.replace(/^http/, "ws")}/graphql`,
+    options: {
+      reconnect: true,
+    },
+    webSocketImpl: typeof WebSocket !== "undefined" ? WebSocket : ws,
+  });
+
+  // Using the ability to split links, you can send data to each link
+  // depending on what kind of operation is being sent.
+  const mainLink = split(
+    // split based on operation type
+    ({ query, operationName }) => {
+      const op = getOperationAST(query, operationName);
+      return (op && op.operation === "subscription") || false;
+    },
+    wsLink,
+    httpLink
+  );
+  return mainLink;
+}
 
 export default withApollo(
-  ({ headers, initialState, ctx }) => {
+  ({ initialState, ctx }) => {
     const ROOT_URL = process.env.ROOT_URL;
     if (!ROOT_URL) {
       throw new Error("ROOT_URL envvar is not set");
     }
-    const isServer = typeof window === "undefined";
+
     const onErrorLink = onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
@@ -28,47 +62,19 @@ export default withApollo(
         );
       if (networkError) console.error(`[Network error]: ${networkError}`);
     });
-    const { req, res }: any = ctx || {};
-    const httpLink =
-      req && res
-        ? new GraphileLink({
-            req,
-            res,
-            postgraphileMiddleware: req.app.get("postgraphileMiddleware"),
-          })
-        : new HttpLink({
-            uri: `${ROOT_URL}/graphql`,
-            credentials: "same-origin",
-            ...(isServer
-              ? { headers: pick(headers, "user-agent", "dnt", "cookie") }
-              : null),
-          });
-    const wsLink = new WebSocketLink({
-      uri: `${ROOT_URL.replace(/^http/, "ws")}/graphql`,
-      options: {
-        reconnect: true,
-      },
-      webSocketImpl: typeof WebSocket !== "undefined" ? WebSocket : ws,
-    });
 
-    // Using the ability to split links, you can send data to each link
-    // depending on what kind of operation is being sent.
-    const mainLink = split(
-      // split based on operation type
-      ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-          definition.kind === "OperationDefinition" &&
-          definition.operation === "subscription"
-        );
-      },
-      wsLink,
-      httpLink
-    );
+    const { req, res }: any = ctx || {};
+    const isServer = typeof window === "undefined";
+    const mainLink =
+      isServer && req && res
+        ? makeServerSideLink(req, res)
+        : makeClientSideLink(ROOT_URL);
+
     const client = new ApolloClient({
       link: ApolloLink.from([onErrorLink, mainLink]),
       cache: new InMemoryCache().restore(initialState || {}),
     });
+
     return client;
   },
   {
