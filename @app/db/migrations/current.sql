9c9f8692edc78ce6ed286b5ecb8876efd995d8a6
Benjie Gillam <benjie@jemjie.com>
2020-02-12 13:25:05 +0000

    Transfer billing contact status on account deletion

--- @app/db/migrations/current.sql
+++ @app/db/migrations/current.sql
@@ -29,3 +29,55 @@ begin
   return null;
 end;
 $$ language plpgsql volatile security definer set search_path to pg_catalog, public, pg_temp;
+
+create or replace function app_public.confirm_account_deletion(token text) returns boolean as $$
+declare
+  v_user_secret app_private.user_secrets;
+  v_token_max_duration interval = interval '3 days';
+begin
+  if app_public.current_user_id() is null then
+    raise exception 'You must log in to delete your account' using errcode = 'LOGIN';
+  end if;
+
+  select * into v_user_secret
+    from app_private.user_secrets
+    where user_secrets.user_id = app_public.current_user_id();
+
+  if v_user_secret is null then
+    -- Success: they're already deleted
+    return true;
+  end if;
+
+  -- Check the token
+  if v_user_secret.delete_account_token = token then
+    -- Token passes
+
+    -- Check that they are not the owner of any organizations
+    if exists(
+      select 1
+      from app_public.organization_memberships
+      where user_id = app_public.current_user_id()
+      and is_owner is true
+    ) then
+      raise exception 'You cannot delete your account until you are not the owner of any organizations.' using errcode = 'OWNER';
+    end if;
+
+    -- Reassign billing contact status back to the organization owner
+    update app_public.organization_memberships
+      set is_billing_contact = true
+      where is_owner = true
+      and organization_id in (
+        select organization_id
+        from app_public.organization_memberships my_memberships
+        where my_memberships.user_id = app_public.current_user_id()
+        and is_billing_contact is true
+      );
+
+    -- Delete their account :(
+    delete from app_public.users where id = app_public.current_user_id();
+    return true;
+  end if;
+
+  raise exception 'The supplied token was incorrect - perhaps you''re logged in to the wrong account, or the token has expired?' using errcode = 'DNIED';
+end;
+$$ language plpgsql strict volatile security definer set search_path to pg_catalog, public, pg_temp;
