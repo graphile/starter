4b62240221059deab60f8f2d2aa7dfcae8a010cf
eleijonmarck <eric.leijonmarck@gmail.com>
2020-02-22 18:44:01 +0100

    Add feed table and drop statements to show reverse drop
    - Added another table for the example to show how to drop tables in
    reverse order at beginning of first save of the file.
    - Battle tested (meaning, a new docker image and everything from scrath
    ran through)
    - lower cased everything as it is in 000.sql commited (and also to be
    SQL 2020 compliant ) >D

--- @app/db/migrations/current.sql
+++ @app/db/migrations/current.sql
@@ -5,28 +5,42 @@ example of a table that could be created after setup complete
 
 - uncomment the statements below
 - save the file and graphile-migrate will update the schema into database automagically
+- any "drop" statements should be at the top (in reverse order)
 */
 
--- CREATE TYPE app_public.post_topic AS enum (
+-- drop table if exists feed;
+-- drop table if exists post;
+
+-- create type app_public.post_topic as enum (
 --   'discussion',
 --   'inspiration',
 --   'help',
 --   'showcase'
 -- );
 
--- CREATE TABLE IF NOT EXISTS app_public.post (
---   id               SERIAL PRIMARY KEY,
---   author_id        INTEGER NOT NULL REFERENCES app_public.users(id),
---   headline         TEXT NOT NULL CHECK (char_length(headline) < 280),
---   body             TEXT,
+-- create table if not exists app_public.post (
+--   id               serial primary key,
+--   author_id        integer not null references app_public.users(id),
+--   headline         text not null check (char_length(headline) < 280),
+--   body             text,
 --   topic            app_public.post_topic,
---   created_at       TIMESTAMP default NOW()
+--   created_at       timestamp default now()
 -- );
 
--- COMMENT ON TABLE APP_PUBLIC.POST IS 'A forum post written by a user.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.ID IS 'The primary key for the post.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.HEADLINE IS 'The title written by the user.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.AUTHOR_ID IS 'The id of the author user.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.TOPIC IS 'The topic this has been posted in.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.BODY IS 'The main body text of our post.';
--- COMMENT ON COLUMN APP_PUBLIC.POST.CREATED_AT IS 'The time this post was created.';
+-- comment on table app_public.post is 'a forum post written by a user.';
+-- comment on column app_public.post.id is 'the primary key for the post.';
+-- comment on column app_public.post.headline is 'the title written by the user.';
+-- comment on column app_public.post.author_id is 'the id of the author user.';
+-- comment on column app_public.post.topic is 'the topic this has been posted in.';
+-- comment on column app_public.post.body is 'the main body text of our post.';
+-- comment on column app_public.post.created_at is 'the time this post was created.';
+
+-- create table if not exists app_public.feed (
+--   id               serial primary key,
+--   posts            integer not null references app_public.post(id),
+--   created_at       timestamp default now()
+-- );
+
+-- comment on table app_public.feed is 'the feed of the posts';
+-- comment on column app_public.feed.id is 'the primary key for the feed.';
+-- comment on column app_public.feed.created_at is 'the time this feed was created.';
