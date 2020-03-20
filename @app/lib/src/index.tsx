e82d23a4d6912ff3d8ef2bb72943baddc7cd1440
Benjie Gillam <benjie@jemjie.com>
2020-03-20 18:56:22 +0000

    Remove outdated export

--- @app/lib/src/index.tsx
+++ @app/lib/src/index.tsx
@@ -1,6 +1,5 @@
 export * from "./GraphileApolloLink";
 export * from "./passwords";
-export * from "./organization";
 export * from "./withApollo";
 export * from "./errors";
 export * from "./forms";
