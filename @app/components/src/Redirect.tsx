35042959312f0098097dcea91627d4c43240d6de
Benjie Gillam <benjie@jemjie.com>
2020-03-23 15:52:29 +0000

    Prettier redirecting page

--- @app/components/src/Redirect.tsx
+++ @app/components/src/Redirect.tsx
@@ -1,13 +1,34 @@
+import { useApolloClient } from "@apollo/react-hooks";
+import { Result, Skeleton } from "antd";
 import Router from "next/router";
 import React, { useEffect } from "react";
+
+import { SharedLayout } from "./SharedLayout";
 
 export interface RedirectProps {
   href: string;
 }
 
 export function Redirect({ href }: RedirectProps) {
+  const client = useApolloClient();
   useEffect(() => {
     Router.push(href);
   }, [href]);
-  return <div>Redirecting...</div>;
+  return (
+    <SharedLayout
+      title="Redirecting..."
+      query={{
+        loading: true,
+        data: undefined,
+        error: undefined,
+        networkStatus: 0,
+        client,
+        refetch: (async () => {
+          throw new Error("Redirecting...");
+        }) as any,
+      }}
+    >
+      <Skeleton />
+    </SharedLayout>
+  );
 }
