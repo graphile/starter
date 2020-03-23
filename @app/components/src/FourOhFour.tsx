989a39220f4334bc5daab55ac4078a1ff6976ad5
Benjie Gillam <benjie@jemjie.com>
2020-03-23 15:47:41 +0000

    New 404

--- @app/components/src/FourOhFour.tsx
+++ @app/components/src/FourOhFour.tsx
@@ -1,25 +1,23 @@
-import { Empty } from "antd";
+import { Empty, Result } from "antd";
 import Link from "next/link";
 import React from "react";
 
+import { ButtonLink } from "./ButtonLink";
 import { H4, P } from "./Text";
 
 export function FourOhFour() {
   return (
     <div>
-      <Empty
-        style={{ marginTop: "2rem" }}
-        description={<H4>Page Not Found</H4>}
-      >
-        <P>
-          The page you attempted to load was not found.
-          <br />
-          Please check the URL and try again, or visit{" "}
-          <Link href="/">
-            <a>the homepage</a>
-          </Link>
-        </P>
-      </Empty>
+      <Result
+        status="404"
+        title="404"
+        subTitle="The page you attempted to load was not found. Maybe you need to log in?"
+        extra={
+          <ButtonLink type="primary" href="/">
+            Back Home
+          </ButtonLink>
+        }
+      />
     </div>
   );
 }
