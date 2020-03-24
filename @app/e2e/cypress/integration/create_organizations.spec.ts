ceefdd0f7cd73d47a0e3727dccf45d567a3fa9e9
Benjie Gillam <benjie@jemjie.com>
2020-03-24 13:25:53 +0000

    Rename test suite

--- @app/e2e/cypress/integration/create_organizations.spec.ts
+++ @app/e2e/cypress/integration/create_organizations.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-context("Organizations", () => {
+context("Create organizations", () => {
   beforeEach(() => cy.serverCommand("clearTestUsers"));
   beforeEach(() => cy.serverCommand("clearTestOrganizations"));
 
