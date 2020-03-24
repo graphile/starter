c187e94deb5599105ac80048bd399caae395bdd6
Benjie Gillam <benjie@jemjie.com>
2020-03-24 10:03:31 +0000

    Make consistent with other spawnSync calls

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,50 @@
 #!/usr/bin/env node
 
 require("@app/config/env");
-const { execSync, spawnSync } = require("child_process");
+const { execSync, spawnSync: rawSpawnSync } = require("child_process");
 const concurrently = require("concurrently");
+
+const spawnSync = (cmd, args, options) => {
+  const result = rawSpawnSync(cmd, args, {
+    stdio: ["pipe", "inherit", "inherit"],
+    env: {
+      ...process.env,
+      YARN_SILENT: "1",
+      npm_config_loglevel: "silent",
+    },
+    ...options,
+  });
+
+  const { error, status, signal, stderr, stdout } = result;
+
+  if (error) {
+    throw error;
+  }
+
+  if (status || signal) {
+    if (stdout) {
+      console.log(stdout.toString("utf8"));
+    }
+    if (stderr) {
+      console.error(stderr.toString("utf8"));
+    }
+    if (status) {
+      throw new Error(
+        `Process exited with status '${status}' (running '${cmd} ${
+          args ? args.join(" ") : ""
+        }')`
+      );
+    } else {
+      throw new Error(
+        `Process exited due to signal '${signal}' (running '${cmd} ${
+          args ? args.join(" ") : null
+        }')`
+      );
+    }
+  }
+
+  return result;
+};
 
 // Dear graphile-migrate, please treat the test DB as if it were the shadow DB
 process.env.SHADOW_DATABASE_URL = process.env.TEST_DATABASE_URL;
@@ -48,9 +90,5 @@ if (process.argv.length > 3) {
   throw new Error(`Argument '${arg}' not understood`);
 } else {
   // Run once, so just run the tests
-  const { status } = spawnSync("jest", ["-i", ...process.argv.slice(2)], opts);
-  if (status !== 0) {
-    console.error(`Jest exited with status: ${status}`);
-    process.exit(status);
-  }
+  spawnSync("jest", ["-i", ...process.argv.slice(2)], opts);
 }
