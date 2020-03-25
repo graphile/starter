657ee9622ee024f2bd6cee037f531cc3297202f9
Benjie Gillam <benjie@jemjie.com>
2020-03-25 09:19:54 +0000

    Use production build on non-development

--- @app/server/src/middleware/installSSR.ts
+++ @app/server/src/middleware/installSSR.ts
@@ -6,7 +6,7 @@ if (!process.env.NODE_ENV) {
   throw new Error("No NODE_ENV envvar! Try `export NODE_ENV=development`");
 }
 
-const isDev = process.env.NODE_ENV !== "production";
+const isDev = process.env.NODE_ENV === "development";
 
 export default async function installSSR(app: Express) {
   // @ts-ignore Next had a bad typing file, they claim `export default` but should have `export =`
