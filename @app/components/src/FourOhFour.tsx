60c7d9d7ebf31e03e974b17ecc5f2dfe85691b0a
Benjie Gillam <benjie@jemjie.com>
2020-03-25 10:43:21 +0000

    Only prompt to log in when not logged in

--- @app/components/src/FourOhFour.tsx
+++ @app/components/src/FourOhFour.tsx
@@ -1,15 +1,22 @@
+import { User } from "@app/graphql";
 import { Result } from "antd";
 import React from "react";
 
 import { ButtonLink } from "./ButtonLink";
 
-export function FourOhFour() {
+interface FourOhFourProps {
+  currentUser?: Pick<User, "id"> | null;
+}
+export function FourOhFour(props: FourOhFourProps) {
+  const { currentUser } = props;
   return (
     <div data-cy="fourohfour-div">
       <Result
         status="404"
         title="404"
-        subTitle="The page you attempted to load was not found. Maybe you need to log in?"
+        subTitle={`The page you attempted to load was not found.${
+          currentUser ? "" : " Maybe you need to log in?"
+        }`}
         extra={
           <ButtonLink type="primary" href="/">
             Back Home
