c52b2eb7df2e9beeea169ebbaa830a1bc136a07f
Benjie Gillam <benjie@jemjie.com>
2020-03-27 11:46:30 +0000

    Better docker reset script

--- docker/scripts/clean-volumes.js
+++ docker/scripts/clean-volumes.js
@@ -2,11 +2,14 @@
 const { execSync } = require("child_process");
 const { basename, dirname, resolve } = require("path");
 
-const projectName = basename(dirname(resolve(__dirname, "..")));
+const projectName = basename(dirname(resolve(__dirname, ".."))).replace(
+  /[^a-z0-9]/g,
+  ""
+);
 
 try {
   execSync(
-    `docker volume rm ${projectName}_db-volume ${projectName}_node_modules-volume ${projectName}_vscode-extensions ${projectName}_devcontainer_db-volume ${projectName}_devcontainer_node_modules-volume ${projectName}_devcontainer_vscode-extensions`,
+    `docker volume rm ${projectName}_vscode-extensions ${projectName}_devcontainer_db-volume ${projectName}_devcontainer_node_modules-volume ${projectName}_devcontainer_vscode-extensions`,
     { stdio: "inherit" }
   );
 } catch (e) {
