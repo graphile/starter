09ff19617acd5bd69f69f2e3e73017ebb356de77
Charley Bodkin <1542740+charlex@users.noreply.github.com>
2020-04-26 01:22:00 -0400

    fix(@app/server): do not prepare next app on each request

--- @app/server/src/middleware/installSSR.ts
+++ @app/server/src/middleware/installSSR.ts
@@ -11,24 +11,26 @@ const isDev = process.env.NODE_ENV === "development";
 export default async function installSSR(app: Express) {
   // @ts-ignore Next had a bad typing file, they claim `export default` but should have `export =`
   // Ref: https://unpkg.com/next@9.0.3/dist/server/next.js
+  console.log('nextjs', '|', 'Installing...');
   const nextApp = next({
     dev: isDev,
     dir: `${__dirname}/../../../client/src`,
     quiet: !isDev,
     // Don't specify 'conf' key
   });
-  const handlerPromise = (async () => {
+  
+  console.log('nextjs', '|', 'Preparing...');
+  try {
     await nextApp.prepare();
-    return nextApp.getRequestHandler();
-  })();
-  // Foo
-  handlerPromise.catch((e) => {
+  } catch (e) {
     console.error("Error occurred starting Next.js; aborting process");
     console.error(e);
     process.exit(1);
-  });
+  }
+  console.log('nextjs', '|', 'Prepared.');
+
+  const handler = nextApp.getRequestHandler();
   app.get("*", async (req, res) => {
-    const handler = await handlerPromise;
     handler(req, res);
   });
 }
