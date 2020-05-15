b4c984bdfeb8d590295dcd6fa6a4e68c4d86cd64
Benjie Gillam <benjie@jemjie.com>
2020-05-15 11:32:49 +0100

    Use 'slugify'

--- @app/components/src/AvatarUpload.tsx
+++ @app/components/src/AvatarUpload.tsx
@@ -8,25 +8,7 @@ import { RcCustomRequestOptions, UploadFile } from "antd/lib/upload/interface";
 import { ApolloError } from "apollo-client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-export function slugify(string: string) {
-  const a =
-    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
-  const b =
-    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------";
-  const p = new RegExp(a.split("").join("|"), "g");
-
-  return string
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
-    .replace(/^-+/, "") // Trim - from start of text
-    .replace(/-+$/, ""); // Trim - from end of text
-}
+import slugify from "slugify";
 
 export function getUid(name: string) {
   const randomHex = () => Math.floor(Math.random() * 16777215).toString(16);
