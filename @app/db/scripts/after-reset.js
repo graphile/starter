eb4b3a70b19ae8c327eb61827c58b06e963800cc
MegaCookie <mail@jessevandervelden.nl>
2020-09-22 17:13:58 +0200

    Create Cross Platform NodeJS Graphile Migrate scripts

---
+++ @app/db/scripts/after-reset.js
@@ -0,0 +1,9 @@
+const { spawn } = require("child_process");
+spawn("yarn", ["workspace", "@app/worker", "install-db-schema"], {
+  stdio: "inherit",
+  env: {
+    ...process.env,
+    DATABASE_URL: process.env.GM_DBURL,
+  },
+  shell: true
+});
