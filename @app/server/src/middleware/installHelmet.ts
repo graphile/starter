f4d5d23ab3f86d36340edaeea3b0b0a67591865b
Benjie Gillam <benjie@jemjie.com>
2020-12-08 16:21:41 +0000

    Enable unsafe-eval in development

--- @app/server/src/middleware/installHelmet.ts
+++ @app/server/src/middleware/installHelmet.ts
@@ -1,6 +1,23 @@
 import { Express } from "express";
 import helmet from "helmet";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default function installHelmet(app: Express) {
-  app.use(helmet());
+  app.use(
+    helmet(
+      isDev
+        ? {
+            // Dev needs 'unsafe-eval' due to
+            // https://github.com/vercel/next.js/issues/14221
+            contentSecurityPolicy: {
+              directives: {
+                ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                "script-src": ["'self'", "'unsafe-eval'"],
+              },
+            },
+          }
+        : {}
+    )
+  );
 }
