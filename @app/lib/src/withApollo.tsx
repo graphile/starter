e6ff48b1a0cd0eb11c406ec907954be370fa0f96
Benjie Gillam <benjie@jemjie.com>
2021-01-22 18:08:11 +0000

    Upgrade PostGraphile; switch to using graphql-ws (#238)

--- @app/lib/src/withApollo.tsx
+++ @app/lib/src/withApollo.tsx
@@ -1,23 +1,78 @@
 import { getDataFromTree } from "@apollo/react-ssr";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
-import { ApolloLink, split } from "apollo-link";
+import {
+  ApolloLink,
+  FetchResult,
+  Observable,
+  Operation,
+  split,
+} from "apollo-link";
 import { onError } from "apollo-link-error";
 import { HttpLink } from "apollo-link-http";
-import { WebSocketLink } from "apollo-link-ws";
-import { getOperationAST } from "graphql";
+import { getOperationAST, GraphQLError, print } from "graphql";
+import { Client, createClient } from "graphql-ws";
 import withApolloBase from "next-with-apollo";
-import { SubscriptionClient } from "subscriptions-transport-ws";
-import ws from "ws";
 
 import { GraphileApolloLink } from "./GraphileApolloLink";
 
-let wsClient: SubscriptionClient | null = null;
+let wsClient: Client | null = null;
+
+class WebSocketLink extends ApolloLink {
+  public request(operation: Operation): Observable<FetchResult> {
+    return new Observable((sink) => {
+      if (!wsClient) {
+        sink.error(new Error("No websocket connection"));
+        return;
+      }
+      return wsClient.subscribe<FetchResult>(
+        { ...operation, query: print(operation.query) },
+        {
+          next: sink.next.bind(sink),
+          complete: sink.complete.bind(sink),
+          error: (err) => {
+            if (err instanceof Error) {
+              sink.error(err);
+            } else if (err instanceof CloseEvent) {
+              sink.error(
+                new Error(
+                  `Socket closed with event ${err.code}` + err.reason
+                    ? `: ${err.reason}` // reason will be available on clean closes
+                    : ""
+                )
+              );
+            } else {
+              sink.error(
+                new Error(
+                  (err as GraphQLError[])
+                    .map(({ message }) => message)
+                    .join(", ")
+                )
+              );
+            }
+          },
+        }
+      );
+    });
+  }
+}
+
+let _rootURL: string | null = null;
+function createWsClient() {
+  if (!_rootURL) {
+    throw new Error("No ROOT_URL");
+  }
+  const url = `${_rootURL.replace(/^http/, "ws")}/graphql`;
+  return createClient({
+    url,
+  });
+}
 
 export function resetWebsocketConnection(): void {
   if (wsClient) {
-    wsClient.close(false, false);
+    wsClient.dispose();
   }
+  wsClient = createWsClient();
 }
 
 function makeServerSideLink(req: any, res: any) {
@@ -29,6 +84,10 @@ function makeServerSideLink(req: any, res: any) {
 }
 
 function makeClientSideLink(ROOT_URL: string) {
+  if (_rootURL) {
+    throw new Error("Must only makeClientSideLink once");
+  }
+  _rootURL = ROOT_URL;
   const nextDataEl = document.getElementById("__NEXT_DATA__");
   if (!nextDataEl || !nextDataEl.textContent) {
     throw new Error("Cannot read from __NEXT_DATA__ element");
@@ -42,14 +101,8 @@ function makeClientSideLink(ROOT_URL: string) {
       "CSRF-Token": CSRF_TOKEN,
     },
   });
-  wsClient = new SubscriptionClient(
-    `${ROOT_URL.replace(/^http/, "ws")}/graphql`,
-    {
-      reconnect: true,
-    },
-    typeof WebSocket !== "undefined" ? WebSocket : ws
-  );
-  const wsLink = new WebSocketLink(wsClient);
+  wsClient = createWsClient();
+  const wsLink = new WebSocketLink();
 
   // Using the ability to split links, you can send data to each link
   // depending on what kind of operation is being sent.
