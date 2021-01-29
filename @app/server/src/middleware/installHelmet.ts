8429213bfdd8b9beaafcd32dfb27adbb35c9074d
Benjie Gillam <benjie@jemjie.com>
2021-01-29 15:57:01 +0000

    Fix connect-src for safari

--- @app/server/src/middleware/installHelmet.ts
+++ @app/server/src/middleware/installHelmet.ts
@@ -1,6 +1,13 @@
 import { Express } from "express";
 import helmet from "helmet";
 
+const tmpRootUrl = process.env.ROOT_URL;
+
+if (!tmpRootUrl || typeof tmpRootUrl !== "string") {
+  throw new Error("Envvar ROOT_URL is required.");
+}
+const ROOT_URL = tmpRootUrl;
+
 const isDevOrTest =
   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
 
@@ -15,6 +22,13 @@ export default function installHelmet(app: Express) {
               directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
                 "script-src": ["'self'", "'unsafe-eval'"],
+                "connect-src": [
+                  "'self'",
+                  // Safari doesn't allow using wss:// origins as 'self' from
+                  // an https:// page, so we have to translate explicitly for
+                  // it.
+                  ROOT_URL.replace(/^http/, "ws"),
+                ],
               },
             },
           }
