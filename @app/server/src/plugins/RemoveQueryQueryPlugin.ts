97bea3a78c9ab88dc362eae455f19be598169e31
Benjie Gillam <benjie@jemjie.com>
2023-02-24 17:10:21 +0000

    Convert plugins to V5

--- @app/server/src/plugins/RemoveQueryQueryPlugin.ts
+++ @app/server/src/plugins/RemoveQueryQueryPlugin.ts
@@ -1,12 +1,16 @@
-import { Plugin } from "postgraphile";
-
-const RemoveQueryQueryPlugin: Plugin = (builder) => {
-  builder.hook("GraphQLObjectType:fields", (fields, build, context) => {
-    if (context.scope.isRootQuery) {
-      delete fields.query;
-    }
-    return fields;
-  });
+const RemoveQueryQueryPlugin: GraphileConfig.Plugin = {
+  name: "RemoveQueryQueryPlugin",
+  version: "0.0.0",
+  schema: {
+    hooks: {
+      GraphQLObjectType_fields(fields, _build, context) {
+        if (context.scope.isRootQuery) {
+          delete fields.query;
+        }
+        return fields;
+      },
+    },
+  },
 };
 
 export default RemoveQueryQueryPlugin;
