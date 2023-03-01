1256f7179d258b57674d3783663da31f3408b8fb
Benjie Gillam <benjie@jemjie.com>
2023-03-01 14:48:57 +0000

    Async rimraf with globbing again

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
-try {
-  const rimraf = require("rimraf");
-
-  rimraf.sync(`${__dirname}/../@app/*/dist`);
-  rimraf.sync(`${__dirname}/../@app/*/tsconfig.tsbuildinfo`);
-  rimraf.sync(`${__dirname}/../@app/client/.next`);
-} catch (e) {
-  console.error("Failed to clean up, perhaps rimraf isn't installed?");
-  console.error(e);
-}
+(async () => {
+  const { default: rimraf } = await import("rimraf");
+  const { globSync } = await import("glob");
+  try {
+    await rimraf(globSync(`${__dirname}/../@app/*/dist`));
+    await rimraf(globSync(`${__dirname}/../@app/*/tsconfig.tsbuildinfo`));
+    await rimraf(globSync(`${__dirname}/../@app/client/.next`));
+    console.log("Deleted");
+  } catch (e) {
+    console.error("Failed to clean up, perhaps rimraf isn't installed?");
+    console.error(e);
+  }
+})();
