a258c30bb36b6de17d82e393353e1307ed007bab
Benjie Gillam <benjie@jemjie.com>
2023-03-09 10:33:22 +0000

    PostGraphile upgradeHandler

--- @app/server/src/middleware/installPostGraphile.ts
+++ @app/server/src/middleware/installPostGraphile.ts
@@ -2,11 +2,11 @@ import { Express } from "express";
 import { postgraphile } from "postgraphile";
 import { grafserv } from "grafserv/express/v4";
 
-import { getHttpServer } from "../app";
+import { getHttpServer, getUpgradeHandlers } from "../app";
 import { getPreset } from "../graphile.config";
 import { getAuthPgPool, getRootPgPool } from "./installDatabasePools";
 
-export default function installPostGraphile(app: Express) {
+export default async function installPostGraphile(app: Express) {
   // const websocketMiddlewares = getWebsocketMiddlewares(app);
   const authPgPool = getAuthPgPool(app);
   const rootPgPool = getRootPgPool(app);
@@ -23,5 +23,15 @@ export default function installPostGraphile(app: Express) {
   app.set("pgl", pgl);
 
   const serv = pgl.createServ(grafserv);
-  serv.addTo(app, httpServer);
+  serv.addTo(app, httpServer, false);
+
+  // Handle websockets
+  const onUpgrade = await serv.getUpgradeHandler();
+  if (onUpgrade) {
+    const upgradeHandlers = getUpgradeHandlers(app);
+    upgradeHandlers.push({
+      check: serv.shouldHandleUpgrade.bind(serv),
+      upgrade: onUpgrade,
+    });
+  }
 }
